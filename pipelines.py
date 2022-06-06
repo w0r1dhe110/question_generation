346df13acfb3564aca2e6016cf6973a2a594188c
w0r1dhe110 <35865922+w0r1dhe110@users.noreply.github.com>
2022-06-06 15:25:48 +0800

    Update pipelines.py
    debug substring

--- pipelines.py
+++ pipelines.py
@@ -140,6 +140,8 @@ class QGPipeline:
                 
                 answer_text = answer_text.strip()
                 answer_text = answer_text.lstrip('<pad>').strip()
+                print("sent:", sent)
+                print("answer_text:", answer_text)
                 ans_start_idx = sent.index(answer_text)
                 
                 sent = f"{sent[:ans_start_idx]} <hl> {answer_text} <hl> {sent[ans_start_idx + len(answer_text): ]}"
